bd370adf998a6f8dc0e3168f31af4b2ce471238a
election-uk <jimanning@Jims-MacBook-Air.local>
2014-11-01 17:12:48 +0000

    inc

--- mapreduceHashtagHour.js
+++ mapreduceHashtagHour.js
@@ -8,7 +8,25 @@ MongoClient.connect(url, function(err, db) {
 	var mrOutput = db.collection('hashtag_by_hour');
 	mrOutput.findOne({ $query: {}, $orderby: { '_id.hour' : -1 }}, {'_id.hour':true}, function(err, lastMr){
 		console.log('err', err);
-		console.log('lastMr', lastMr);
+		console.log('lastMr', lastMr._id.hour);
+
+		var query = {ts: {$gt: lastMr._id.hour}};
+
+		console.log ('query ', query);
+		var mapFn = function(){
+			var hour = this.ts.substr(0,13)+':00:00.000Z';
+			this.entities.hashtags.forEach(function(h){
+				emit({hour: hour , hashtag: h.text.toLowerCase()}, 1);
+			});
+		};
+
+		var redFn = function(hashtag, count){
+			return Array.sum(count);
+		};
+
+
+
+
 	})
          
 
@@ -23,16 +41,7 @@ MongoClient.connect(url, function(err, db) {
 
 
 
-var mapFn = function(){
-	var hour = this.ts.substr(0,13)+':00:00.000Z';
-	this.entities.hashtags.forEach(function(h){
-		emit({hour: hour , hashtag: h.text.toLowerCase()}, 1);
-	});
-}
 
-var redFn = function(hashtag, count){
-	return Array.sum(count);
-}
 
 // run the job ebery hour to populate an hour by hour table
 // at the same time inremenatlly run a m/r for a totals table
